f338cd00f07a4b9d35060e8f3f21e658191605f2
Daniel Nunez <161102831+danie1nunez@users.noreply.github.com>
2024-07-22 15:07:57 -0400

    added the beginning routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,2 +1,19 @@
+const controllers = require("../../controllers/userC");
 const router = require('express').Router();
 
+const {
+    getallUsers,
+    getUser,
+    createUser,
+    deleteUser,
+    updateUser,
+} = require("../../controllers/userC");
+
+router.route('/').get(getallUsers).post(createUser)
+
+router.route('/:userId').get(getUser).put(updateUser).delete(deleteUser)
+
+router.route('/:userId/friends/friendId').post().delete()
+
+
+module.exports = router;
