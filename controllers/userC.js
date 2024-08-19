e8785d043e9ce7029d4841cd3d0bbdf911fc2675
Daniel Nunez <161102831+danie1nunez@users.noreply.github.com>
2024-08-19 15:37:57 -0400

    fixed up the last routes

--- controllers/userC.js
+++ controllers/userC.js
@@ -4,36 +4,41 @@ const { Thought, User } = require('../models');
 module.exports = {
     async getallUsers(req,res) {
         try{
-          const users = await User.find();
+          const users = await User.find().populate('thoughts').populate('friends');
           res.status(200).json(users);
         } catch (err) {
           res.status(500).json(err);
         }
     },
+
     async getUser(req,res) {
         try{
-            const user = await User.findOne({_id: new ObjectId(req.params.id)}).select('-__v');
-           
-            res.json(200)(user);
+            console.log(req.params);
+            const user = await User.findById(req.params.userId).populate('thoughts').populate('friends');
+            console.log(user);
+            res.json(user);
         } catch (err) {
+          console.log(err);
           res.status(500).json(err);
         }
     },
+
     async createUser(req,res) {
         try{
           const user = await User.create(req.body);
-          res.json(200)(user);
+          res.json(user);
         } catch (err) {
           res.status(500).json(err);
         }
     },
+
     async deleteUser(req,res) {
         try{
-          const user = await User.findOneAndDelete({_id: new ObjectId(req.params.id)});
+          const user = await User.findOneAndDelete({_id: new ObjectId(req.params.userId)});
           if (!user) {
             return res.status(404).json({ message: 'No such user exists' });
           }
-          res.status(200).json(user);
+          res.status(200).json({ message: 'user deleted' });
         } catch (err) {
           res.status(500).json(err);
         }
@@ -41,7 +46,11 @@ module.exports = {
 
     async updateUser(req,res) {
       try{
-        const user = await User.findOneAndUpdate({_id: new ObjectId(req.params.id)});
+        const user = await User.findOneAndUpdate(
+          {_id: req.params.userId},
+          { $set: req.body },
+          { runValidators: true, new: true }
+          ).populate('thoughts').populate('friends');
         if (!user) {
           return res.status(404).json({ message: 'No such user exists' });
         }
@@ -54,12 +63,32 @@ module.exports = {
     async addFriend(req,res) {
       console.log('you have added a new friend');
       try{
-        const user = await User.findOne({_id: new ObjectId(req.params.id)});
-        const friend = await User.findOne({_id: new ObjectId(req.params.id)});
-        if (!user) { 
-          return res.status(404).json({ message: 'No such user exists' });
+        const friend = await User.findOneAndUpdate(
+          {_id: req.params.userId},
+          {$addToSet: {friends: new ObjectId(req.params.friendId)}},
+          { runValidators: true, new: true }
+        );
+        if (!friend) { 
+          return res.status(404).json({ message: 'no friend to add' });
         }
-        res.status(200).json(user);
+        res.status(200).json(friend);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    },
+
+    async deleteFriend(req,res) {
+      console.log('you have deleted a friend');
+      try{
+        const friend = await User.findOneAndUpdate(
+          {_id: req.params.userId},
+          {$pull: {friends: new ObjectId(req.params.friendId)}},
+          { runValidators: true, new: true }
+        );
+        if (!friend) { 
+          return res.status(404).json({ message: 'no friend to delete' });
+        }
+        res.status(200).json({ message: 'friend deleted successfully' });
       } catch (err) {
         res.status(500).json(err);
       }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,2 +1,23 @@
 const controllers = require("../../controllers/thoughtC");
 const router = require('express').Router();
+
+const {
+    getallThoughts,
+    getThought,
+    createThought,
+    deleteThought,
+    updateThought,
+    thoughtReaction,
+    deleteReaction
+} = require("../../controllers/thoughtC");
+
+router.route('/').get(getallThoughts).post(createThought)
+
+router.route('/:thoughtId').get(getThought).put(updateThought).delete(deleteThought)
+
+router.route('/:thoughtId/reactions').post(thoughtReaction)
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction)
+
+
+module.exports = router;
